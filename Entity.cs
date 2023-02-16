f187fd5ca2cda012fd72a23225d4828446f85ac3
J319s0 <josh.smith17090@gmail.com>
2023-02-17 02:56:50 +0800

    create entity
    created entity with basic properties and display it on random position on plane

--- Entity.cs
+++ Entity.cs
@@ -1,9 +1,19 @@
 using System;
 
-namespace Simulation.Entity
+namespace Simulation.Entities
 {
     public class Entity
     {
-        
+        public readonly char appearance = 'O';
+        public int[] position = {0,0};
+
+        public int[] Move()
+        {
+            Random rnd = new();
+            int[] coord = new int[2];
+            coord[0] = rnd.Next(0,25);
+            coord[1] = rnd.Next(0,25);
+            return coord;
+        }
     }
 }
--- SimManager.cs
+++ SimManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using Simulation.Entities;
 
 namespace Simulation
 {
@@ -9,31 +10,40 @@ namespace Simulation
         readonly static string path = "output/output.txt";
         public static void Main()
         {
-            string[,] plane = new string[25,25];
+            char[,] plane = new char[25,25];
             int lim1 = plane.GetLength(0);
             int lim2 = plane.GetLength(1);
             plane = CreatePlane(plane, lim1, lim2);
-            for(int i=0; i<lim1; i++) //50
-            {
-                for(int j=0; j<lim2; j++)
-                {
-                    File.AppendAllText(path, plane[i,j]);
-                    File.AppendAllText(path, "  ");
-                }
-                File.AppendAllText(path, "\n");
-            }
+            Entity entity = new();
+            entity.position = entity.Move();
+            plane[entity.position[0],entity.position[1]] = entity.appearance;
+
+            OutputResult(plane, lim1, lim2);
         }  
 
-        public static string[,] CreatePlane(string[,] plane, int lim1, int lim2)
+        public static char[,] CreatePlane(char[,] plane, int lim1, int lim2)
         {
             for(int i=0; i<lim1; i++)
             {
                 for(int j=0; j<lim2; j++)
                 {
-                    plane[i,j] = "0";
+                    plane[i,j] = '+';
                 }
             }
             return plane;
+        }
+
+        public static void OutputResult(char[,] plane, int lim1, int lim2)
+        {
+            for(int i=0; i<lim1; i++) //50
+            {
+                for(int j=0; j<lim2; j++)
+                {
+                    File.AppendAllText(path, plane[i,j].ToString());
+                    File.AppendAllText(path, "  ");
+                }
+                File.AppendAllText(path, "\n");
+            }
         }
     }
 }
