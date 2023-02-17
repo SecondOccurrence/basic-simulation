1227595a01ecc3c2f729d73273f4311e8aee93d8
J319s0 <josh.smith17090@gmail.com>
2023-02-17 19:21:33 +0800

    Update SimManager.cs
    add try catch block when opening file in the case that an error occurs

--- SimManager.cs
+++ SimManager.cs
@@ -8,7 +8,7 @@ namespace Simulation
 {
     public class SimManager
     {
-        readonly static string path = "output/output.txt";
+        readonly static string path = "output/fd.txt";
         public static void Main()
         {
             SimPlane plane = new();
@@ -21,12 +21,19 @@ namespace Simulation
 
         public static void OutputResult(char[,] plane, int lim1, int lim2)
         {
-            for(int i=0; i<lim1; i++) //50
+            for(int i=0; i<lim1; i++)
             {
                 for(int j=0; j<lim2; j++)
                 {
-                    File.AppendAllText(path, plane[i,j].ToString());
-                    File.AppendAllText(path, "  ");
+                    try
+                    {
+                        File.AppendAllText(path, plane[i,j].ToString());
+                        File.AppendAllText(path, "  ");
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
                 }
                 File.AppendAllText(path, "\n");
             }
